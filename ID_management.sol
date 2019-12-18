19b95d01297739e66c786ecafd61c7004cf970e1
Shekhar Jain <sjshekharjain63@gmail.com>
2019-12-18 23:51:07 +0000

    added greylist
    Signed-off-by: Shekhar Jain <sjshekharjain63@gmail.com>

--- ID_management.sol
+++ ID_management.sol
@@ -1,20 +1,21 @@
 pragma solidity >=0.4.22 <0.6.0;
 
-contract Access {
+contract Identity {
 
     struct DID {
         uint8 id;
         bool whitelist;
         bool blacklist;
+        bool greylist;
     }
 
     mapping(address => mapping(address => DID)) devices;
 
     function createID(address owner, address device, uint8 id) public {
-        if (id == 0) {
             devices[owner][device].id = id;
-             devices[owner][device].whitelist = true;
-        }
+            devices[owner][device].whitelist = true;
+            devices[owner][device].blacklist = false;
+            devices[owner][device].greylist = false;
     }
 
     function getID(address owner, address device) public view returns (uint8) {
@@ -29,14 +30,30 @@ contract Access {
             return devices[owner][device].blacklist;
         }
 
-    function checkID(address owner, address device, int id) public view returns (bool) {
+    function checkID(address owner, address device, uint8 id) public view returns (bool) {
             return devices[owner][device].id == id;
     }
 
+    function blacklistDevice(address owner, address device) public {
+            devices[owner][device].blacklist = true;
+            devices[owner][device].whitelist = false;
+            devices[owner][device].greylist = false;
+    }
+
+    function whitelistDevice(address owner, address device) public {
+            devices[owner][device].blacklist = false;
+            devices[owner][device].whitelist = true;
+            devices[owner][device].greylist = false;
+    }
+
+     function greylistDevice(address owner, address device) public {
+            devices[owner][device].blacklist = false;
+            devices[owner][device].whitelist = false;
+            devices[owner][device].greylist = true;
+    }
+
     function updateID(address owner, address device, uint8 newID) public {
-        if (newID == 0) {
             devices[owner][device].id = newID;
-        }
     }
 
     function deleteDevice(address owner, address device) public {
