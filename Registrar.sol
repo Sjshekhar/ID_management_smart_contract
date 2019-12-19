3637fade1e7c84d9a090bb2d2ea2025dbed1eec2
Shekhar Jain <sjshekharjain63@gmail.com>
2019-12-19 00:20:26 +0000

    made changes
    Signed-off-by: Shekhar Jain <sjshekharjain63@gmail.com>

--- Registrar.sol
+++ Registrar.sol
@@ -9,65 +9,67 @@ contract Registrar {
         bool whitelist;
         bool blacklist;
         bool greylist;
+        string businessModel;
     }
 
     mapping(address => identity) register;
 
-    function registerOrganisation(address owner, uint8 id, string memory name, uint8 noOfDevices) public {
-            register[owner].id = id;
-            register[owner].name = name;
-            register[owner].whitelist = true;
-            register[owner].blacklist = true;
-            register[owner].noOfDevices = noOfDevices;
+    function registerOrganisation(address organisation, uint8 id, string memory name, uint8 noOfDevices, string memory businessModel) public {
+            register[organisation].id = id;
+            register[organisation].name = name;
+            register[organisation].whitelist = true;
+            register[organisation].blacklist = true;
+            register[organisation].noOfDevices = noOfDevices;
+            register[organisation].businessModel = businessModel;
     }
 
-    function getOrganisationID(address owner) public view returns (uint8) {
-            return register[owner].id;
+    function getOrganisationID(address organisation) public view returns (uint8) {
+            return register[organisation].id;
         }
 
-    function isOrganisationWhitelisted(address owner) public view returns (bool) {
-            return register[owner].whitelist;
+    function isOrganisationWhitelisted(address organisation) public view returns (bool) {
+            return register[organisation].whitelist;
         }
 
-     function isOrganisationBlacklisted(address owner) public view returns (bool) {
-            return register[owner].blacklist;
+     function isOrganisationBlacklisted(address organisation) public view returns (bool) {
+            return register[organisation].blacklist;
         }
 
-    function checkID(address owner, uint8 id) public view returns (bool) {
-            return register[owner].id == id;
+    function checkID(address organisation, uint8 id) public view returns (bool) {
+            return register[organisation].id == id;
     }
 
-    function blacklistCompany(address owner) public {
-            register[owner].blacklist = true;
-            register[owner].whitelist = false;
-            register[owner].greylist = false;
+    function blacklistCompany(address organisation) public {
+            register[organisation].blacklist = true;
+            register[organisation].whitelist = false;
+            register[organisation].greylist = false;
     }
 
-    function whitelistCompany(address owner) public {
-            register[owner].blacklist = false;
-            register[owner].whitelist = true;
-            register[owner].greylist = false;
+    function whitelistCompany(address organisation) public {
+            register[organisation].blacklist = false;
+            register[organisation].whitelist = true;
+            register[organisation].greylist = false;
     }
 
-    function greylistCompany(address owner) public {
-            register[owner].blacklist = false;
-            register[owner].whitelist = false;
-            register[owner].greylist = true;
+    function greylistCompany(address organisation) public {
+            register[organisation].blacklist = false;
+            register[organisation].whitelist = false;
+            register[organisation].greylist = true;
     }
 
-    function updateID(address owner, uint8 newID) public {
-            register[owner].id = newID;
+    function updateID(address organisation, uint8 newID) public {
+            register[organisation].id = newID;
     }
 
-    function updateName(address owner, string memory name) public {
-            register[owner].name = name;
+    function updateName(address organisation, string memory name) public {
+            register[organisation].name = name;
     }
 
-    function deleteOrganisation(address owner) public {
-            delete register[owner];
+    function deleteOrganisation(address organisation) public {
+            delete register[organisation];
         }
 
-    function updateNoOfDevices(address owner, uint8 newNoOfDevices) public {
-            register[owner].noOfDevices = newNoOfDevices;
+    function updateNoOfDevices(address organisation, uint8 newNoOfDevices) public {
+            register[organisation].noOfDevices = newNoOfDevices;
         }
 }
